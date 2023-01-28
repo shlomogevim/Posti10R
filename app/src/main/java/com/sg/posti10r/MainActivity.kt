5402b262187fc4a756e428025e5ccdca3c3bfe25
shlomo <shlomo.gevim@gmail.com>
2023-01-28 15:20:32 +0200

    demi 28/1/2023 15:30

--- app/src/main/java/com/sg/posti10r/MainActivity.kt
+++ app/src/main/java/com/sg/posti10r/MainActivity.kt
@@ -5,8 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,22 +19,56 @@ class MainActivity : AppCompatActivity() {
 
 
         val posts = Posts4Lines().createPosts()
-//        logi("   size = ${posts.size} ")
+         createRecyclerView(posts)
 //        posts.forEach { logi("${it.postText.toString()}") }
 
+
+    }
+    private fun createRecyclerView(posts: ArrayList<Post>) {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+       // val adapter = PostAdapter(posts)
+//        recyclerView.adapter = adapter
+        recyclerView.adapter = FlipCardAdapter(posts)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
+    }
+
+
+
+
+
+
+   /* private fun createRecyclerView(posts: ArrayList<Post>) {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val adapter = PostAdapter(posts)
         recyclerView.adapter = adapter
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.layoutManager = layoutManager
+    }*/
+    /* private fun create_rvPost() {
+        val layoutManger = CenterZoomLayout(this)
+        layoutManger.orientation = LinearLayoutManager.HORIZONTAL
+        layoutManger.reverseLayout = true
+        // layoutManger.stackFromEnd=true
+        rvPosts.layoutManager = layoutManger
+
+//        val snapHelper = LinearSnapHelper()
+//        rvPosts.setOnFlingListener(null)
+//        snapHelper.attachToRecyclerView(rvPosts)
+
+        val snapHelper1=PagerSnapHelper()
+        rvPosts.setOnFlingListener(null)
+        snapHelper1.attachToRecyclerView(rvPosts)
 
 
-
-       // val currentPost = FindPost().individualPost()
-        // logi("${currentPost.postNum}   ")
-      //  drawPostHelper.drawPost( constraintLayout, currentPost)
-
-    }
+        rvPosts.isNestedScrollingEnabled = false
+        postAdapter = PostAdapter(this, posts)
+        rvPosts.adapter = postAdapter
+        rvPosts.setHasFixedSize(true)
+        postAdapter.notifyDataSetChanged()
+    }*/
 
     fun logi(message: String) {
         Log.i("gg", message)
